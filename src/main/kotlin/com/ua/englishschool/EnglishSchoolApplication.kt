233c3f2e4a3addcaa03d248e3928457bdf390c4a
Igor Krivoy <javadevik@gmail.com>
2022-07-18 19:44:33 +0300

    Inital commit

---
+++ src/main/kotlin/com/ua/englishschool/EnglishSchoolApplication.kt
@@ -0,0 +1,11 @@
+package com.ua.englishschool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnglishSchoolApplication
+
+fun main(args: Array<String>) {
+	runApplication<EnglishSchoolApplication>(*args)
+}
---
+++ src/test/kotlin/com/ua/englishschool/EnglishSchoolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ua.englishschool
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EnglishSchoolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
