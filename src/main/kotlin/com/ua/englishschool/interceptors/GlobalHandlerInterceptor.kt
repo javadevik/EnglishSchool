bc5362fc1bf3274bcbd0261e727174b7f3db28c5
javadevik <javadevik@gmail.com>
2022-08-20 20:09:11 +0300

    writed GlobalGandlerInterceptor

--- src/main/kotlin/com/ua/englishschool/interceptors/GlobalHandlerInterceptor.kt
+++ src/main/kotlin/com/ua/englishschool/interceptors/GlobalHandlerInterceptor.kt
@@ -9,10 +9,10 @@ import javax.servlet.http.HttpServletResponse
 class GlobalHandlerInterceptor : HandlerInterceptor {
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         val authentication = SecurityContextHolder.getContext().authentication
-        if (!authentication.isAuthenticated) {
-            return false
+        if (authentication.isAuthenticated) {
+            val userDetails = authentication.principal as UserDetails
+            return userDetails.isEnabled
         }
-        // TODO write logic of check user status
         return true
     }
 }
